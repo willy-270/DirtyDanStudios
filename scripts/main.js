426e808dc7f9079a8456611c750f0ad8be7da1c4
willy-270 <jjarlo1985@gmail.com>
2024-01-16 11:53:04 -0600

    ROCK PAPER SCISSORS

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,66 @@
-function randomNum() {
-    if (document.getElementById('randomNumP')) {
-        document.getElementById('randomNumP').remove();
+function playGame(playerChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+   let playerEmoji;
+
+    if (playerChoice === "rock") {
+        playerEmoji = "🪨";
+    }
+    else if (playerChoice === "paper") {
+        playerEmoji = "📜";
+    }
+    else if (playerChoice === "scissors") {
+        playerEmoji = "✂️";
     }
 
-    const randomNum = Math.floor(Math.random() * 10) + 1;
-    const body = document.getElementsByTagName('body')[0];
+    let computerEmoji;
 
-    const pNum = document.createElement('p');
-    pNum.setAttribute('id', 'randomNumP');
-    pNum.textContent = "Random Number = " + randomNum;
+    if (computerChoice === "rock") {
+        computerEmoji = "🪨";
+    }
+    else if (computerChoice === "paper") {
+        computerEmoji = "📜";
+    }
+    else if (computerChoice === "scissors") {
+        computerEmoji = "✂️";
+    }
+    
+    let result = "You: " + playerEmoji + " Computer: " + computerEmoji + ", ";
 
-    body.appendChild(pNum);
+    if (playerChoice === computerChoice) {
+        result += "It's a tie!";
+    } else if (playerChoice === "rock") {
+        if (computerChoice === "scissors") {
+            result += "You win!";
+        } else {
+            result += "You lose!";
+        }
+    }
+    else if (playerChoice === "paper") {
+        if (computerChoice === "rock") {
+            result += "You win!";
+        } else {
+            result += "You lose!";
+        }
+    }
+    else if (playerChoice === "scissors") {
+        if (computerChoice === "paper") {
+            result += "You win!";
+        } else {
+            result += "You lose!";
+        }
+    }
+
+    let resultText;
+
+    if (document.getElementById("rps-result") === null) {
+        const body = document.querySelector("body");
+        resultText = document.createElement("p");
+        resultText.setAttribute("id", "rps-result");
+        body.appendChild(resultText);
+    }
+
+    resultText = document.getElementById("rps-result");
+    resultText.textContent = result;
 }
